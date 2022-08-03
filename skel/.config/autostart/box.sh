700b5b52a0ea9ddff997ab57f1a8729a7658a75b
freedomos <frazergrant345@gmail.com>
2022-08-03 20:41:19 +0100

    upload

---
+++ skel/.config/autostart/box.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+zenity \
+--info \
+--text="FreedomOS is still in <b>Development</b>. \nExpect bugs with the OS or Apps. \nIssues can be logged here. \nhttps://github.com/freedomos/Applications/issues" \
+--title="FreedomOS Message" \
+--ok-label="Continue"
