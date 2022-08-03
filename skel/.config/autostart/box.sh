620698b8f0085c17e3df5b843ab32bbea2b0de99
freedomos <frazergrant345@gmail.com>
2022-08-03 23:30:26 +0100

    remove

--- skel/.config/autostart/box.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-zenity \
---info \
---text="FreedomOS is still in <b>Development</b>. \nExpect bugs with the OS or Apps. \nIssues can be logged here. \nhttps://github.com/freedomos/Applications/issues" \
---title="FreedomOS Message" \
---ok-label="Continue"
